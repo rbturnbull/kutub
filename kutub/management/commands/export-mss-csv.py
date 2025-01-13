ed1ac6c2c175edf59ef9e3b7b299cec64e6dd477
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2025-01-13 14:46:02 +1100

    :zap: working on export command

--- kutub/management/commands/export-mss-csv.py
+++ kutub/management/commands/export-mss-csv.py
@@ -8,4 +8,5 @@ class Command(BaseCommand):
         parser.add_argument('csv', type=str, default="", help="A filepath to export to.")
 
     def handle(self, *args, **options):
-        export.all_manuscripts_to_csv(options['csv'])
+        with open(options['csv'], "w") as f:
+            export.all_manuscripts_to_csv(f)
